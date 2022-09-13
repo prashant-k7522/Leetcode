de347d11757bccb392a82deb093c00014a476fc7
prashant-k7522 <107603237+prashant-k7522@users.noreply.github.com>
2022-09-13 09:28:30 +0530

    Time: 71 ms (31.21%), Space: 42.2 MB (99.27%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -2,19 +2,20 @@
 //index+value=jump_value
 class Solution {
     public int jump(int[] nums) {
-     int res = 0;
-        int l = 0; int r = 0;
-        int farthest = 0;
-        while(r < nums.length-1)
-        {
-            for(int i = l; i < r + 1; i++)
-            {
-                farthest = Math.max(farthest,nums[i]+i);
+      int n=nums.length;
+      int[] dp=new int[n];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[n-1]=0;
+        
+        for(int i=n-2;i>=0;i--){
+            int min=Integer.MAX_VALUE;
+            for(int j=i+1;j<=Math.min(n-1,i+nums[i]);j++){
+                min=Math.min(min,dp[j]);
             }
-            l = r + 1;
-            r = farthest;
-            res++;
+            if(min!=Integer.MAX_VALUE){
+                dp[i]=min+1;
+            }
         }
-        return res;
+        return dp[0];
         }
     }
